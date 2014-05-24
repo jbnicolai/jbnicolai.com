13ab7f5f14db63b9756af04a42bd6f5e966ce274
Joshua Appelman <jappelman@xebia.com>
2014-05-25 00:35:55 +0200

    Splits sass config from Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,36 +34,6 @@ module.exports = function (grunt) {
       app: 'app',
       dist: 'dist'
     },
-    sass: {
-      options: {
-        bundleExec: true,
-        debugInfo: false,
-        lineNumbers: false,
-        loadPath: 'app/_bower_components'
-      },
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/_scss',
-          src: '**/*.{scss,sass}',
-          dest: '.tmp/css',
-          ext: '.css'
-        }]
-      },
-      server: {
-        options: {
-          debugInfo: true,
-          lineNumbers: true
-        },
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/_scss',
-          src: '**/*.{scss,sass}',
-          dest: '.tmp/css',
-          ext: '.css'
-        }]
-      }
-    },
     autoprefixer: {
       options: {
         browsers: ['last 2 versions']
