b0bc125a426caeece74665557fa9388d0dd9e8ed
Joshua Appelman <jappelman@xebia.com>
2014-05-25 00:45:37 +0200

    Splits filerev config from Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,21 +39,6 @@ module.exports = function (grunt) {
     // Usemin adds files to uglify
     uglify: {},
     // Usemin adds files to cssmin
-    filerev: {
-      options: {
-        length: 4
-      },
-      dist: {
-        files: [{
-          src: [
-            '<%= yeoman.dist %>/js/**/*.js',
-            '<%= yeoman.dist %>/css/**/*.css',
-            '<%= yeoman.dist %>/img/**/*.{gif,jpg,jpeg,png,svg,webp}',
-            '<%= yeoman.dist %>/fonts/**/*.{eot*,otf,svg,ttf,woff}'
-          ]
-        }]
-      }
-    },
     buildcontrol: {
       dist: {
         options: {
