248ed478fd82b52c7e6e29f38094965724bd127b
Joshua Appelman <jappelman@xebia.com>
2014-05-25 13:04:44 +0200

    Changes hostname to 0.0.0.0.

--- grunt/config/connect.js
+++ grunt/config/connect.js
@@ -2,8 +2,7 @@ module.exports =  {
   options: {
     port: 9000,
     livereload: 35729,
-    // change this to '0.0.0.0' to access the server from outside
-    hostname: 'localhost'
+    hostname: '0.0.0.0'
   },
   livereload: {
     options: {
