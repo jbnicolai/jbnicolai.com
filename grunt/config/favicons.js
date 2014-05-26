1d69bcb5863b5bc59daee3543da47c3e5651a68a
Joshua Appelman <jappelman@xebia.com>
2014-05-27 00:36:16 +0200

    Adds favicons grunt config.

---
+++ grunt/config/favicons.js
@@ -0,0 +1,11 @@
+module.exports = {
+  options: {
+    windowsTile: false
+    //html: '<%= paths.app %>/_layouts/default.html',
+    //HTMLPrefix: '/img/icons/'
+  },
+  icons: {
+    src: '<%= paths.app %>/_assets/favicon.png',
+    dest: '<%= paths.dist %>/img/icons'
+  }
+};
